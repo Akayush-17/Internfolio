2c1db1996d546913d3c4d34d40bb78adc6c1f8e0
Z3RO-O <vaibhav.singh@fyle.in>
2025-10-05 12:51:56 +0530

    feat: move auth page before form filling

---
+++ src/assets/assets.ts
@@ -0,0 +1,11 @@
+import bag from "./bag.png";
+import headphone from "./headphone.png";
+import laptops from "./laptops.png";
+import document from "./document.png";
+import window from "./window.svg";
+import globe from "./globe.svg";
+import file from "./file.svg";
+import github from "./github.svg";
+import google from "./google.svg";
+
+export { bag, headphone, laptops, document, window, globe, file, github, google };
