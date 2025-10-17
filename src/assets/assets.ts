e961f76aa9803bdfa950a6c91460c9eec2a8656d
Z3RO-O <vaibhav.singh@fyle.in>
2025-10-17 11:00:21 +0530

    update

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -3,5 +3,9 @@ import github from './github.svg';
 import project from './project.svg';
 import learning from './learning.svg';
 import review from './review.svg';
+import laptop from './laptops.png';
+import headphone from './headphone.png';
+import bag from './bag.png';
+import document from './document.png';
 
-export { google, github, project, learning, review };
+export { google, github, project, learning, review, laptop, headphone, bag, document };
